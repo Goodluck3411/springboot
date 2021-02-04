b918f2b404b7f380f26083132650afe7b06df629
tom_pro <goodMorning_pro@atiguigu.com>
2021-02-04 11:50:26 +0800

    Initial commit

---
+++ src/main/java/com/sun/SpringbootApplication.java
@@ -0,0 +1,14 @@
+package com.sun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+//程序的主入口
+@SpringBootApplication
+public class SpringbootApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(SpringbootApplication.class, args);
+    }
+
+}
